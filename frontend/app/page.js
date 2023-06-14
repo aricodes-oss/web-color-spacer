3ea660606e293be0f9c9e1c58cae7d3b236c77b1
Aria Taylor <ari@aricodes.net>
2023-06-13 20:09:41 -0400

    Add green rectangle

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
       <div className={styles.row}>
         <div className={classNames(styles.rectangle, styles.blue)} />
         <div className={classNames(styles.rectangle, styles.red)} />
+        <div className={styles.rectangle} style={{ backgroundColor: 'rgb(0, 255, 0)' }} />
       </div>
 
       <button onClick={() => alert('You did it!')}>Press me!</button>
