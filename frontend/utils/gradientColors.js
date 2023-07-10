6d39180bfe4fe460a405b8ca43c3da739034aa51
Keiran Jensen <keiranjensen@gmail.com>
2023-07-10 18:40:45 -0400

    Rename gradient interval factor

--- frontend/utils/gradientColors.js
+++ frontend/utils/gradientColors.js
@@ -1,12 +1,12 @@
-export default function gradientColors(coefficients, offset, scale = 17) {
+export default function gradientColors(coefficients, offset, baseInterval = 17) {
   const [xAxis, yAxis] = Object.keys(coefficients);
   const point = { r: offset, g: offset, b: offset };
 
   const rows = [];
 
-  for (let i = 0; i < 255; i += scale / Math.sqrt(coefficients[xAxis])) {
+  for (let i = 0; i < 255; i += baseInterval / Math.sqrt(coefficients[xAxis])) {
     const points = [];
-    for (let j = 0; j < 255; j += scale / Math.sqrt(coefficients[yAxis])) {
+    for (let j = 0; j < 255; j += baseInterval / Math.sqrt(coefficients[yAxis])) {
       points.push({ ...point, [xAxis]: Math.trunc(i), [yAxis]: Math.trunc(j) });
     }
     rows.push(points);
