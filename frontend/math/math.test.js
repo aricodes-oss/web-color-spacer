2f9ae548edb1c00dbe3ae330984fbfe76755d0bb
Aria Taylor <ari@aricodes.net>
2023-08-08 17:17:37 -0400

    Add test showing multiple expression evaluation

--- frontend/math/math.test.js
+++ frontend/math/math.test.js
@@ -6,3 +6,7 @@ test('has bidirectional srgb/xyz transforms', () => {
     math.evaluate('srgb_to_xyz(xyz_to_srgb(input)) == input', { input }).every(Boolean),
   ).toBeTruthy();
 });
+
+test('accepts multiple statements at once', () => {
+  expect(math.evaluate(['f(x) = x^2', 'f(8)']).pop()).toBe(64);
+});
